d95c334b524a79f99a11e16e5332362bc861e4a1
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-07-05 15:16:05 -0400

    set up realtionships between users, books, and ratings

---
+++ app/Book.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
+}
