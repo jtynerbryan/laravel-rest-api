b27fbd1e17deaff8354b6577ffaee20ad97f0add
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-07-05 15:20:47 -0400

    setup models for mass assignment and installed jwt auth package

--- app/Book.php
+++ app/Book.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    protected $fillable = [
+        'user_id', 'title', 'description',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- app/Rating.php
+++ app/Rating.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
+    protected $fillable = [
+        'user_id', 'book_id', 'rating',
+    ];
+
     public function book()
     {
         return $this->belongsTo(Book::class);
